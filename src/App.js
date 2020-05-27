4ba16c9a36589264e8215e1564694db875751461
Narmadhu <oknarmadhu10496@gmail.com>
2020-05-27 08:45:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="todo-form">
+        <input placeholder="Your todos..." className="submit-bar" />
+        <button type="submit" className="submit-btn">
+          Add
+        </button>
+        <ul className="todo-list">
+          <li className="todo-items">
+            <a className="delete">X</a>
+          </li>
+        </ul>
+      </form>
     </div>
   );
 }
