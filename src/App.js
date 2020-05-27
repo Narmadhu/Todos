832ccfacd1d1245fd09d65db42604db110432179
Narmadhu <oknarmadhu10496@gmail.com>
2020-05-27 11:44:22 +0530

    deploy todo app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [itemsList, setItemsList] = useState([
+    { key: 1, text: "Watering plants" },
+    { key: 2, text: "Do Yoga" },
+  ]);
+
+  const updateNewTodo = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const getNewTodo = (e) => {
+    e.preventDefault();
+    if (newTodo === "") return;
+    setItemsList([{ key: Date.now(), text: newTodo }, ...itemsList]);
+    e.target.reset();
+  };
+
+  const removeTodo = (key) => {
+    setItemsList(itemsList.filter((itemList) => itemList.key !== key));
+  };
+
   return (
     <div className="App">
-      <form className="todo-form">
-        <input placeholder="Your todos..." className="submit-bar" />
+      <form className="todo-form" onSubmit={getNewTodo}>
+        <input
+          placeholder="Your todos..."
+          className="submit-bar"
+          onChange={updateNewTodo}
+        />
         <button type="submit" className="submit-btn">
           Add
         </button>
         <ul className="todo-list">
-          <li className="todo-items">
-            <a className="delete">X</a>
-          </li>
+          {itemsList.map((itemList) => (
+            <li key={itemList.key} className="todo-items">
+              {itemList.text}
+              <a className="delete" onClick={() => removeTodo(itemList.key)}>
+                X
+              </a>
+            </li>
+          ))}
         </ul>
       </form>
     </div>
